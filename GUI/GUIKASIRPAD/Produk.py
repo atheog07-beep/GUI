b79371d730c7f0e68aae705f3ee999373ee1246f
atheog07-beep <atheog07@gmail.com>
2026-06-05 15:25:49 +0700

    Update Produk.py
    ath07

--- GUI/GUIKASIRPAD/Produk.py
+++ GUI/GUIKASIRPAD/Produk.py
@@ -1,20 +1,20 @@
-import pandas as pd
-import os
-class Produk:
-    def __init__(self, file_produk):
-        base_dir = os.path.dirname(os.path.abspath(__file__))
-        self.file_produk = os.path.join(base_dir, file_produk)
-        self.daftar      = self.baca()
+import pandas as pd#import library pandas alias pd
+import os#import modul os
+class Produk:#inisialisasi class Produk
+    def __init__(self, file_produk):#constructor berparameter file produk
+        base_dir = os.path.dirname(os.path.abspath(__file__))#ambil directory file
+        self.file_produk = os.path.join(base_dir, file_produk)#gabung directory dengan nama file
+        self.daftar      = self.baca()#panggil method bava()
 
-    def baca(self):
-        df = pd.read_csv(self.file_produk) 
-        df = df.rename(columns={"nama_barang": "nama"})
-        df["kode"]  = df["kode"].astype(str)
-        df["harga"] = df["harga"].astype(int)
-        return df.to_dict(orient="records")
+    def baca(self):#method untuk membaca file produk csv
+        df = pd.read_csv(self.file_produk) #baca file csv ke dalam dataframe pd
+        df = df.rename(columns={"nama_barang": "nama"})#rename kolom agar seragam
+        df["kode"]  = df["kode"].astype(str)#konversi kolom kode ke String
+        df["harga"] = df["harga"].astype(int)#konversi kolom harga ke integer
+        return df.to_dict(orient="records")#konversi dataframe ke list of dict
 
-    def cari(self, nama):
-        for item in self.daftar:
-            if item["nama"] == nama:
-                return item
-        return None
+    def cari(self, nama):#method untuk mencari produk berdasarkan nama
+        for item in self.daftar:#iterasi setiap item dalam produk
+            if item["nama"] == nama:#jika nama cocok
+                return item#return data produk
+        return None#jika tidak, none
